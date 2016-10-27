ab10ea17b49c89f9ead21afcecdd0e51b2710c3d
Jose Antonio Alvarez Nieto <josete1409@gmail.com>
2016-10-27 18:14:22 +0200

    1

--- Proyecto_FCT_Android/src/Prueba_Hola_Mundo.java
+++ Proyecto_FCT_Android/src/Prueba_Hola_Mundo.java
@@ -4,7 +4,7 @@ public class Prueba_Hola_Mundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
+		// Ya he sido capaz de hacer la subida del Proyecto e integración con gitHub //
 		
 	}
 
